4a7fa309f8bbd23c0022516c252b39f73aa3ed32
Katie Hawthorne <kathryne.h@gmail.com>
2013-03-18 22:49:38 -0400

    organizing folders, putting authenticate in utils

--- index.php
+++ index.php
@@ -3,23 +3,25 @@
 	require_once("utils/dbconnect.php");
 	require_once("utils/api.php");
 	require_once("templates/display_labels.php");
-	require_once("authenticate.php");
+	require_once("utils/authenticate.php");
 	$db=getdb();
 	
 	
 	session_start();
-	$auth = new authenticate();
+	//$auth = new authenticate();
 	
-	if(isset($_SESSION['user_id'])) {
-	    header('Location: login.php');
-	}
-	
-	else {
-	    $logged_in = $auth->checkSession();
-	    if(empty($logged_in)) {
-		$auth->logout();
-		header('Location: login.php');
-	    } else {
+	//TODO the authenticate class seems to be buggy
+
+	// if(!isset($_SESSION['user_id'])) {
+	//     header('Location: login.php');
+	// }
+	// else {
+	//     $logged_in = $auth->checkSession();
+	//     if(empty($logged_in)) {
+	// 		$auth->logout();
+	// 		header('Location: login.php');
+	//     } 
+	//     else {
 	    	$upc = '003000005586';
 			echo "<section id='container'>";
 		    echo "<div class='row'>";
@@ -32,13 +34,8 @@
 		    echo "</div>";
 	        echo "</section>";
 	    }
-        }
+    }
 	
-
-
-	/* Paste code between *** /n *** and delete commenters to enable login prompting.
-	 * Note: this function requires a login to see the main content of the verification app. */
- 
 		//TODO: add guest option 
 
 	include("templates/footer.php");	
