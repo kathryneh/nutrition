0b7254c7fbdff3fae40b6102d2947946401c284f
Katie Hawthorne <kathryne.h@gmail.com>
2013-03-19 19:12:41 -0400

    chasing down javascript bugs on dynamically changing things

--- js/nutrution.js
+++ js/nutrution.js
@@ -11,6 +11,7 @@ var submitNutritionLabel = function(){
 	return false;
 };
 
+//TODO fix this "fixed position" bug....
 $(window).scroll(function (event) {
 	var windowTop = $(this).scrollTop();
 	if (windowTop+520 >= $("footer").offset().top) {
@@ -20,23 +21,54 @@ $(window).scroll(function (event) {
 	}
 });
 
+var hideRow = function(event){
+	//event will be on the foundicon-remove - need to hide the row
+	$(event.target).closest("tr").toggleClass("hidden");
+	//and create a new button below
+	var clickedIcon = $(event.target)[0];
+	console.log($(event.target)[0].getAttribute("data"));
+	var colName = clickedIcon.getAttribute("data");
+	var colText = "";
+	if ((colText = clickedIcon.parentNode.parentNode.children[0].children[0].innerHTML) != 'undefined'){
+		colText = clickedIcon.parentNode.parentNode.children[0].children[0].innerHTML;
+	}
+	else{
+		colText = lickedIcon.parentNode.parentNode.children[0].innerHTML;
+	}
+	var outerDiv = document.createElement("div");
+	$(outerDiv).addClass("button");
+	$(outerDiv).addClass("add");
+	outerDiv.setAttribute("column", colName);
+	var innerIcon = document.createElement("i");
+	$(innerIcon).addClass("foundicon-plus");
+	outerDiv.appendChild(innerIcon);
+	outerDiv.appendChild(document.createTextNode(colText));
+	$(".hiddenRows").append(outerDiv);
+};
+
+var unhideRow = function(event, columnName){
+	//event will be on element .add; need to hide the button
+	//
+	//and unhide the row above
+};
+
+
 $(document).ready(function(){
 	var colName;
 	$(".hiddenRows").on('click', function(event){
-		if($(event.target).hasClass("add")){
+		if($(event.target).hasClass("add") || $(event.target).hasClass("foundicon-plus")  || event.target.parentNode.classList.contains("add")){
 			colName = event.target.getAttribute("column");
+			unhideRow(event, colName);
 			$("tr[data-column='"+colName+"']").toggleClass("hidden");
 			$(event.target).toggleClass("hidden");
 		}
 	});
 
 	$(".data").on('click', function(event){
+		console.log($(event.target)[0]);
 		if($(event.target).hasClass("foundicon-remove")){
-			$(event.target).parent().$(parent()).toggleClass("hidden");
-			var test = $(event.target).parent().siblings("input").getAttribute("name");
-			console.log(test);
+			hideRow.bind(this);
+			hideRow(event);
 		}
-		var newIcon = document.createDocumentFragment("<div class='button add' column='$key'><i class='foundicon-plus'></i> $columnArray[$key]</div>");
-		$(".hiddenRows").append(newIcon);
 	});
 });
